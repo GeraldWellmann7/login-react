ba8b1a5ea3766fbf9144374679db4bacca6f7e46
GeraldWellmann7 <gwellmann1997@gmail.com>
2023-08-11 21:35:11 -0400

    update2

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { auth } from "..//firebase";
 import { Home } from "../components/Home";
 import { Login } from "../components/Login";
